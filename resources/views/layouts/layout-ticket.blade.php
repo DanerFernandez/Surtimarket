12b5c0513a3e6325e38278f5de95560fa3ed232b
Daniel Fernandez <daniel.ernesto.fernandez.flores@gmail.com>
2020-10-10 20:39:47 -0500

    15

--- resources/views/layouts/layout-ticket.blade.php
+++ resources/views/layouts/layout-ticket.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <link rel="shortcut icon" type="image/png" href="../resources\views\imagenes\small-business-160.png"/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
